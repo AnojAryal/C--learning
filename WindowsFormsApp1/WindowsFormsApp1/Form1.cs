109012e538ffc23f8e35a612a59753120b95ab7f
anojaryal <anoj1810@gmail.com>
2023-09-23 19:19:28 +0545

    build first formapp

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            int num1, num2;
+
+            if (int.TryParse(textBox1.Text, out num1) && int.TryParse(textBox2.Text, out num2))
+            {
+
+                int sum = num1 + num2;
+
+
+                label1.Text = "Result: " + sum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers in both textboxes.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
