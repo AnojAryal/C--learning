6da7bdf05a3af70af7ccd757d145932930ff6ca3
anojaryal <anoj1810@gmail.com>
2023-10-03 14:19:55 +0545

    week 4

---
+++ FileI0/FileI0/FileIO1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileIO
+{
+    internal class FileIO1
+    {
+        public void WriteContents()
+        {
+            try
+            {
+                FileInfo file = new FileInfo(@"data1.txt");
+                FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine("Line-1");
+                sw.Close();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void  AppendContents()
+        {
+            try
+            {
+                StreamWriter sw = File.AppendText(@"data1.txt");
+                sw.WriteLine("Line-2");
+                sw.Close();
+
+            }
+            catch (Exception) { }
+        }
+        public void ReadContents()
+        {
+            try
+            {
+                FileInfo file = new FileInfo(@"data1.txt");
+                FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+                StreamReader sr = new StreamReader(fs);
+                String contents = sr.ReadToEnd();
+                sr.Close();
+                Console.WriteLine(contents);
+            }
+            catch (Exception ex) {
+                
+                Console.WriteLine(ex.Message); 
+            }
+        }
+
+    }
+}
---
+++ FileI0/FileI0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+new FileIO.FileIO1().WriteContents();
+
+
+// new FileIO.FileIO1().ReadContents();
